c36ff26fa5af9638bd6a3f33ff69fb8e33e7c5df
William Morgans <61888773+wmorgans@users.noreply.github.com>
2023-02-08 14:18:09 +0000

    Update conf.py

--- docs/conf.py
+++ docs/conf.py
@@ -36,8 +36,7 @@ extensions = [
   'sphinx.ext.duration',
   'sphinx.ext.doctest',
   'sphinx.ext.autodoc',
-  'sphinx.ext.autosummary',
-  'furo'
+  'sphinx.ext.autosummary'
 ]
 
 # Add any paths that contain templates here, relative to this directory.
